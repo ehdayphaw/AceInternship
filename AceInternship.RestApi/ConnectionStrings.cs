3ead07ad1bb8d75a90e6e3414ee7c8b43e5cf4d3
nawehdayphaw <nawehdayphaw.2020@gmail.com>
2024-06-20 15:26:48 +0630

    [Complete] EF Core Rest Api

---
+++ AceInternship.RestApi/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AceInternship.RestApi
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "AceInternship",
+            UserID = "sa",
+            Password = "edp@123",
+            TrustServerCertificate = true
+        };
+
+    }
+}
--- AceInternship.RestApi/Controllers/BlogController.cs
+++ AceInternship.RestApi/Controllers/BlogController.cs
@@ -1,102 +1,110 @@
-﻿using Dapper;
+﻿using AceInternship.RestApi.Db;
+using AceInternship.RestApi.Models;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace AceInternship.RestApi.Controllers
+namespace AceInternship.RestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly AppDbContext _context;
+    public BlogController()
     {
-        private readonly SqlConnectionStringBuilder _connectionStringBuilder;
-        public BlogController()
-        {
-            /* _connectionStringBuilder = new SqlConnectionStringBuilder();
-             _connectionStringBuilder.DataSource = "."; //Server Name
-             _connectionStringBuilder.InitialCatalog = "AceInternship"; //Database Name
-             _connectionStringBuilder.UserID = "sa";
-             _connectionStringBuilder.Password = "edp@123";*/
+        _context = new AppDbContext();
+    }
+    [HttpGet]
+    public IActionResult Read()
+    {
+        var lst=_context.Blogs.ToList();
+      
+        return Ok(lst);
+    }
 
-            _connectionStringBuilder = new SqlConnectionStringBuilder()
-            {
-                DataSource = ".",
-                InitialCatalog = "AceInternship",
-                UserID = "sa",
-                Password="edp@123"
-            };
-        }
-        [HttpGet]
-        public IActionResult GetBlogs()
+    [HttpGet("{id}")]
+    public IActionResult Edit(int id)
+    {
+       var item= _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
         {
-           
-            using IDbConnection db=new SqlConnection(_connectionStringBuilder.ConnectionString);
-            var lst=db.Query<TblBlog>(Queries.BlogList).ToList();
-            return Ok(lst);
-        }
-        [HttpGet("{id}")]
-        public IActionResult GetBlogs(int id)
-        {
-
-            using IDbConnection db = new SqlConnection(_connectionStringBuilder.ConnectionString);
-            var item = db.Query<TblBlog>(Queries.BlogById, new { BlogId = id}).FirstOrDefault();
-            if(item is null)
-                return NotFound("No data Found.");
-
-            return Ok(item);
+            return NotFound("Data not found.");
         }
 
-        [HttpPost]
-       
-        public IActionResult   CreateBlog(TblBlog blog)
-        {
-            using IDbConnection db=new SqlConnection(_connectionStringBuilder.ConnectionString);
-            int result =db.Execute(Queries.BlogCreate, blog);
-            string message = result > 0 ? "Saving Success" : "Saving Fail";
-            return Ok(message);
-        }
+        return Ok(item);
+    }
 
-        [HttpPut("{id}")]
-        public IActionResult UpdateBlog(int id,TblBlog  blog)
-        {
-            blog.BlogId = id;
-            using IDbConnection db = new SqlConnection(_connectionStringBuilder.ConnectionString);
-            int result =db.Execute(Queries.BlogUpdate, blog);
-            string msg = result > 0 ? "Update Success" : "Update Fail";
+    [HttpPost]
+    public IActionResult Create(BlogModel blogModel)
+    {
+            _context.Blogs.Add(blogModel);
+            int result = _context.SaveChanges();
+            string msg = result > 0 ? "Create Success" : "Create Fail";
             return Ok(msg);
-        }
-
-        [HttpPatch]
-        public IActionResult PatchBlog()
-        {
-            return Ok("PatchBlog");
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult DeleteBlog(int id)
-        {
-            using IDbConnection db = new SqlConnection(_connectionStringBuilder.ConnectionString);
-            int result=db.Execute(Queries.BlogDelete, new { BlogId = id });
-            string msg = result > 0 ? "Delete Success" : "Delete Fail";
-            return Ok(msg);
-
-        }
     }
-
-    public class TblBlog
+    [HttpPut("{id}")]
+    public IActionResult Update(int id,BlogModel blogModel)
     {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
+       var item= _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            return NotFound("Data not found");
+        }
+        item.BlogTitle = blogModel.BlogTitle;
+        item.BlogAuthor = blogModel.BlogAuthor;
+        item.BlogContent = blogModel.BlogContent;
+        int result = _context.SaveChanges();
+        string msg = result > 0 ? "Update Success" : "Update Fail";
+        return Ok(msg);
     }
-    public static class Queries
+
+    [HttpPatch]
+    public IActionResult Patch(int id,BlogModel blogModel)
     {
-        public static string BlogList { get; } = @"SELECT [BlogId], [BlogTitle], [BlogAuthor], [BlogContent] FROM [dbo].[Tbl_Blog]";
-        public static string BlogById { get; } = @"SELECT [BlogId], [BlogTitle], [BlogAuthor], [BlogContent] FROM [dbo].[Tbl_Blog] WHERE BlogId = @BlogId";
-        public static string BlogCreate { get; } = @"INSERT INTO [dbo].[Tbl_Blog] ([BlogTitle], [BlogAuthor], [BlogContent]) VALUES (@BlogTitle, @BlogAuthor, @BlogContent)";
-        public static string BlogUpdate { get; } = @"UPDATE [dbo].[Tbl_Blog] SET [BlogTitle] = @BlogTitle, [BlogAuthor] = @BlogAuthor, [BlogContent] = @BlogContent WHERE [BlogId] = @BlogId";
-        public static string BlogDelete { get; } = @"DELETE FROM [dbo].[Tbl_Blog] WHERE BlogId = @BlogId";
+        var item=_context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            return NotFound("Data not found.");
+        }
+        if (!string.IsNullOrEmpty(blogModel.BlogTitle))
+        {
+            item.BlogTitle=blogModel.BlogTitle;
+        }
+        if (!string.IsNullOrEmpty(blogModel.BlogAuthor))
+        {
+            item.BlogAuthor = blogModel.BlogAuthor;
+        }
+        if (!string.IsNullOrEmpty(blogModel.BlogContent))
+        {
+            item.BlogContent = blogModel.BlogContent;
+        }
+
+        int result = _context.SaveChanges();
+        string msg = result > 0 ? "Update Success" : "Update Fail";
+        return Ok(msg);
     }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        var item= _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if(item is null)
+        {
+            return NotFound("data not found");
+        }
+      _context.Remove(item);
+        int result =_context.SaveChanges();
+      string msg = result > 0 ? "Delete Success" : "Delete Fail";
+        return Ok(msg);
+    }
+
+
+
+
+
 }
+
+
